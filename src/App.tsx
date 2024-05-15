cf7d5beb1968fe6c51a8f63fb94d825f4af526bc
prhmkm <prhm_km@yahoo.com>
2024-05-15 19:25:12 +0330

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { Col, Container, Row } from "react-bootstrap";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Container>
+          <Row>
+            <Col>
+              <h1>Welcome to GWM chatroom</h1>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/waitingroom.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { Col, Form, Row } from "react-bootstrap";
+
+const waitingroom = () => {
+  const [username, setUsername] = useState("");
+  const [chatroom, setChatroom] = useState("");
+  return (
+    <Form
+      onSubmit={(e) => {
+        e.preventDefault();
+        // joinChatRoom(username, chatroom);
+      }}
+    >
+      <Row>
+        <Col>
+          <Form.Group>
+            <Form.Control
+              placeholder="UserName"
+              onChange={(e) => setUsername(e.target.value)}
+            ></Form.Control>
+            <Form.Control
+              placeholder="ChatRoom"
+              onChange={(e) => setChatroom(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+        </Col>
+      </Row>
+    </Form>
+  );
+};
+
+export default waitingroom;
