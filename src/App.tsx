30b20c28c6f233b3a79ac5f3fc9dbd3068395512
prhmkm <prhm_km@yahoo.com>
2024-05-23 01:29:33 +0330

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Col, Container, Row } from "react-bootstrap";
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Waitingroom from "./components/waitingroom";
@@ -7,6 +7,9 @@ import { HubConnectionBuilder, LogLevel } from "@microsoft/signalr";
 
 function App() {
   const [conn, setConnection] = useState<any>();
+  //const [_chatRoom,_setChatRoom] = useState("");
+  const [_message,_setMessage] = useState("");
+
 
   const joinChatRoom = async (username: string, chatroom: string) => {
     try {
@@ -16,6 +19,25 @@ function App() {
         .build();
 
       conn.on("JoinToChatRoom", (user, msg) => {
+        console.log("msg : ", msg);
+      });
+
+      await conn.start();
+      await conn.invoke("JoinToChatRoom", { username, chatroom });
+
+      setConnection(conn);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  const SendMessageToGroup = async (_message: string) => {
+    try {
+      const conn = new HubConnectionBuilder()
+        .withUrl("https://localhost:7168/Chat")
+        .configureLogging(LogLevel.Information)
+        .build();
+
+      conn.on("SendMessageToGroup", (user, msg) => {
         console.log("msg : ", msg);
       });
 
@@ -38,6 +60,30 @@ function App() {
             </Col>
           </Row>
           <Waitingroom joinChatRoom={joinChatRoom}></Waitingroom>
+    <Form
+      onSubmit={(e) => {
+        e.preventDefault();
+        SendMessageToGroup(_message);
+        //https://www.youtube.com/watch?v=pvi_ZS_PrSc
+      }}
+    >
+      <Row className="px-5 py-5">
+        <Col sm={12}>
+          <Form.Group>
+            <Form.Control
+              placeholder="Message"
+              onChange={(e) => _setMessage(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+        </Col>
+        <Col sm={12}>
+          <hr />
+          <Button variant="success" type="submit">
+            Join
+          </Button>
+        </Col>
+      </Row>
+    </Form>
         </Container>
       </div>
     </>
